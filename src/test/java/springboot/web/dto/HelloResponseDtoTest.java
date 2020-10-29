e652727c3bfde0b8f629cf594ed13561e1301157
eunchong <chd830@naver.com>
2020-10-29 16:52:34 +0900

    add lombok and TDD

---
+++ src/test/java/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package springboot.web.dto;
+
+import com.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void lombok() {
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // assertj라는 테스트 검증 라이브러리의 검증 메소드
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
